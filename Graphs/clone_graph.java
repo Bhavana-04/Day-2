a993d700afaf2a1c8b8805f020bd63d231cd2056
Bhavana-04 <72656179+Bhavana-04@users.noreply.github.com>
2021-01-19 11:05:50 +0530

    Add files via upload

---
+++ Graphs/clone_graph.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Node cloneGraph(Node node) {
+        //if node is null return it
+        if(node==null)
+            return node;
+        //create a queue for storing a nodes
+        Queue<Node> q=new LinkedList<>();
+        //create a hash map for storing the all neighbors of a node
+        HashMap<Node,Node> hm=new HashMap<>();
+        q.add(node);
+        Node n=new Node(node.val);
+        hm.put(node,n);
+        while(!q.isEmpty()){
+            Node curr=q.poll();
+            //iterate all neighbors
+            for(Node adj:curr.neighbors){
+                //if it is not int the hashmap then add in to a queue and map
+                if(!hm.containsKey(adj)){
+                    Node newNode=new Node(adj.val);
+                    hm.put(adj,newNode);
+                    q.add(adj);
+                }
+                hm.get(curr).neighbors.add(hm.get(adj));
+            }
+        }
+        return hm.get(node);
+    }
+}
